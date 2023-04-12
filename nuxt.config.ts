44702a319f6676756cb46755101dced48503f641
rusty.donuthole <rusty.donuthole@protonmail.com>
2023-04-11 19:49:37 -0500

    added devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ["~/assets/styles.css"],
     // ssr: false,
+    css: ["~/assets/styles.css"],
+    devtools: true,
 });
