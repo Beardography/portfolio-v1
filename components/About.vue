207dc941b6d922b4523ea9316bb4e7ff99ba7f6c
rusty.donuthole <rusty.donuthole@protonmail.com>
2023-05-01 19:54:49 -0500

    added a new class and updated styles

--- components/About.vue
+++ components/About.vue
@@ -6,7 +6,7 @@
             <img
                 src="https://images.unsplash.com/photo-1549692520-acc6669e2f0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
             />
-            <p>
+            <!-- <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis
                 ut diam quam nulla porttitor massa. Porttitor massa id neque
@@ -28,7 +28,27 @@
                 elementum facilisis leo vel fringilla. Vel elit scelerisque
                 mauris pellentesque pulvinar pellentesque habitant. Nibh
                 venenatis cras sed felis eget velit.
-            </p>
+            </p> -->
+
+            <article class="content">
+                <p>
+                    I'm a UI Designer and Developer with a passion for designing
+                    and building user interfaces. I have a background in graphic
+                    design and over time took up front-end development. I enjoy
+                    working with teams to create beautiful and functional
+                    interfaces. I enjoy the idea of designing a project and then
+                    building it out. I'm always looking to learn new things and
+                    improve my skills.
+                </p>
+
+                <p>
+                    Outside of work I enjoy spending time on my ereader,
+                    spending time with friends and family, and playing video
+                    games. I also enjoy expanding my knowledge and use of Linux
+                    (I use Arch btw) as well as playing around with open-source
+                    projects, and coming up with ideas for future projects.
+                </p>
+            </article>
         </section>
     </div>
 </template>
